2f61fb58e221527be41367e4dbe7b49d5ac2b0a6
anokhimehta <anokhimehta00@gmail.com>
2025-10-12 22:11:50 -0400

    problem 2

--- LCBC_HW2.py
+++ LCBC_HW2.py
@@ -30,3 +30,51 @@ class Solution:
             index+=1
 
         return sign * total_int
+
+########FIND ALL ANAGRAMS IN STRING
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+        answer = []
+        #create dict of p
+        p_letters = {}
+        for c in p:
+            if c not in p_letters:
+                p_letters[c] = 1
+            else:
+                p_letters[c] += 1
+
+        window = len(p)
+
+        #create initial window dictionary
+        substr = s[0: window]
+        s_letters = {}
+        for c in substr:
+            if c not in s_letters:
+                s_letters[c] = 1
+            else:
+                s_letters[c] += 1
+        
+        if (p_letters == s_letters):
+            answer.append(0)
+
+        for i in range(1, len(s) - window + 1):
+            #1. remove the first letter of the previous window
+            first_letter = s[i - 1]
+            s_letters[first_letter] -= 1
+            if s_letters[first_letter] == 0:
+                del s_letters[first_letter]
+            #2. add the new letter to the window
+            new_letter = s[i + window - 1]
+            if new_letter not in s_letters:
+                s_letters[new_letter] = 1
+            else:
+                s_letters[new_letter] += 1
+
+            if (p_letters == s_letters):
+                answer.append(i)
+
+        return answer
+
+
+        
