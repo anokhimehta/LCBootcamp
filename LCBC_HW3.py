1d7979b0fa67f271ec3cfaf69637fc5ced49bd92
anokhimehta <anokhimehta00@gmail.com>
2025-10-17 02:09:06 -0400

    did #2

--- LCBC_HW3.py
+++ LCBC_HW3.py
@@ -32,3 +32,36 @@ class Solution:
 
         return True
         
+####Reorder List
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        #get fast to end
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        #reverse second half
+        second = slow.next
+        slow.next = None
+        node = None
+
+        while second:
+            temp = second.next
+            second.next = node
+            node = second
+            second = temp
+
+        #merge two halves
+        first = head
+        second = node
+
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next, second.next, = second, temp1
+            first, second = temp1, temp2
+        
