f362d94069b1bb01a930974f505c52ceef70bc24
anokhimehta <anokhimehta00@gmail.com>
2025-11-02 23:10:44 -0500

    Add dailyTemperatures

--- LCBC_HW4.py
+++ LCBC_HW4.py
@@ -74,3 +74,25 @@ class Solution:
 
         return total
             
+
+
+####DAILY TEMPERATURES
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        #setup array
+        arr = [0] * len(temperatures)
+
+        #stack will contain tuples (temp, index)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                stack_temp, stack_index = stack.pop()
+                arr[stack_index] = i - stack_index
+
+            stack.append((t, i))
+
+        return arr
+
+        
